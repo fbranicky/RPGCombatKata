00428c6afe36541c4989c24f98bf83f6aa562dd4
fbranicky <fbranicky@aztekweb.com>
2016-07-29 21:22:32 -0400

    Iteration 1.1

---
+++ RPGCombatKata/Character.cs
@@ -0,0 +1,24 @@
+﻿namespace RPGCombatKata
+{
+    public class Character
+    {
+        private const int StartingHealth = 1000;
+        private const int StartingLevel = 1;
+        private const bool StartingAlive = true;
+
+        private int _health;
+        public int Health { get { return _health; } }
+        private int _level;
+        public int Level { get { return _level; } }
+        public bool _alive;
+        public bool Alive { get { return _alive; } }
+
+
+        public Character()
+        {
+            _health = StartingHealth;
+            _level = StartingLevel;
+            _alive = StartingAlive;
+        }
+    }
+}
---
+++ RPGCombatKataTests/CharacterWhenCreatedShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using RPGCombatKata;
+
+namespace RPGCombatKataTests
+{
+    public class CharacterWhenCreatedShould
+    {
+        [Test]
+        public void HaveHealthAt1000()
+        {
+            var character = new Character();
+            character.Health.ShouldBe(1000);
+        }
+
+        [Test]
+        public void HaveLevelAt1()
+        {
+            var character = new Character();
+            character.Level.ShouldBe(1);
+        }
+
+        [Test]
+        public void BeAlive()
+        {
+            var character = new Character();
+            character.Alive.ShouldBe(true);
+        }
+    }
+}
